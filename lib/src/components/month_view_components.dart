30805b4a344c935e9ac22f5531d037e12be60ccf
Aryan Ghorpade <124383198+aryangh21@users.noreply.github.com>
2024-09-07 19:11:09 +0530

    Update month_view_components.dart

--- lib/src/components/month_view_components.dart
+++ lib/src/components/month_view_components.dart
@@ -212,7 +212,7 @@ class FilledCell<T extends Object?> extends StatelessWidget {
                                       fontSize:
                                           MediaQuery.of(context).size.width >=
                                                   600
-                                              ? 12
+                                              ? 10
                                               : 8,
                                     ),
                               ),
@@ -255,7 +255,7 @@ class FilledCell<T extends Object?> extends StatelessWidget {
                                                           .size
                                                           .width >=
                                                       600
-                                                  ? 12
+                                                  ? 10
                                                   : 8,
                                             ),
                                       ),
